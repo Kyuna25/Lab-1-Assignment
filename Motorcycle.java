7def7cb714c4a34104b1f5e9cf8b4eb05325a327
Kyuna25 <kyunanancy@gmail.com>
2026-04-14 15:28:28 +0300

    Create Motorcycle.java

---
+++ Motorcycle.java
@@ -0,0 +1,11 @@
+class Motorcycle extends Vehicle{
+  void accelerate() {
+    System.out.printLn("Motorcycle is moving");
+  }
+  void stop() {
+    System.out.printLn("Motorcycle has stopped");
+  }
+  void gas() {
+    System.out.printLn("Motorcycle Refuiling");
+  }
+}
