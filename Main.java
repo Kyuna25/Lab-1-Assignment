604ced2b8969d037d2c20b0a46954f0d92018575
Kyuna25 <kyunanancy@gmail.com>
2026-04-14 15:44:59 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Sedan sedan = new Sedan();
+
+        sedan.accelerate();       
+        sedan.accelerate(120);      
+        sedan.stop();
+        sedan.gas();
+    }
+}
