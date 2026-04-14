222436f1ec12ad846bf2f4edbcb5476e5355ea70
Kyuna25 <kyunanancy@gmail.com>
2026-04-14 15:31:34 +0300

    Create Bus.java

---
+++ Bus.java
@@ -0,0 +1,11 @@
+class Bus extends Vehicle{
+  void accelerate() {
+    System.out.printLn("Bus is moving");
+  }
+  void stop() {
+    System.out.printLn("Bus has stopped");
+  }
+  void gas() {
+    System.out.printLn("Bus is refuiling");
+  }
+}
